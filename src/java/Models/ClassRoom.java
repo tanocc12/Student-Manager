5023a46fb7afa6e17b203753ededa5642af94e5c
tanocc12 <tans8205@gmail.com>
2026-07-03 16:28:24 +0700

    Update ClassRoom annd Course

--- src/java/Models/ClassRoom.java
+++ src/java/Models/ClassRoom.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Models;
 
-/**
- *
- * @author tans8
- */
 public class ClassRoom {
-    
+    private int id;
+    private String classCode;
+    private String className;
+    private int majorId;
+    private String majorName; 
+
+    public ClassRoom() {}
+
+    public ClassRoom(int id, String classCode, String className, int majorId) {
+        this.id = id;
+        this.classCode = classCode;
+        this.className = className;
+        this.majorId = majorId;
+    }
+
+    // Getter và Setter
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getClassCode() { return classCode; }
+    public void setClassCode(String classCode) { this.classCode = classCode; }
+
+    public String getClassName() { return className; }
+    public void setClassName(String className) { this.className = className; }
+
+    public int getMajorId() { return majorId; }
+    public void setMajorId(int majorId) { this.majorId = majorId; }
+
+    public String getMajorName() { return majorName; }
+    public void setMajorName(String majorName) { this.majorName = majorName; }
 }
--- src/java/Models/Course.java
+++ src/java/Models/Course.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Models;
 
-/**
- *
- * @author tans8
- */
+
 public class Course {
+    private int id;
+    private String courseCode;
+    private String courseName;
+    private int credits;
+
+    public Course(int id, String courseCode, String courseName, int credits) {
+        this.id = id;
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.credits = credits;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
     
 }
