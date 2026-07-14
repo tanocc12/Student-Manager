20c959fca44c9c6b9e474051ac4576db95c29bed
tanocc12 <tans8205@gmail.com>
2026-07-14 14:34:12 +0700

    update register

--- src/java/Controller/RegisterServlet.java
+++ src/java/Controller/RegisterServlet.java
@@ -1,13 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controller;
 
-/**
- *
- * @author tans8
- */
-public class RegisterServlet {
-    
+import DAO.UserDAO;
+import Models.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.getRequestDispatcher("register.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+
+        String fullName = request.getParameter("fullName");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String confirmPassword = request.getParameter("confirmPassword");
+        String role = request.getParameter("role");
+
+        UserDAO dao = new UserDAO();
+
+        if (!password.equals(confirmPassword)) {
+
+            request.setAttribute("error",
+                    "Confirm password does not match.");
+
+            request.getRequestDispatcher("register.jsp")
+                    .forward(request, response);
+            return;
+        }
+
+        if (dao.checkEmail(email)) {
+
+            request.setAttribute("error",
+                    "Email already exists.");
+
+            request.getRequestDispatcher("register.jsp")
+                    .forward(request, response);
+            return;
+        }
+
+        User user = new User();
+
+        user.setFullName(fullName);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRole(role);
+
+        if (dao.register(user)) {
+
+            response.sendRedirect(request.getContextPath() + "/login");
+
+        } else {
+
+            request.setAttribute("error",
+                    "Register failed.");
+
+            request.getRequestDispatcher("register.jsp")
+                    .forward(request, response);
+        }
+    }
 }
